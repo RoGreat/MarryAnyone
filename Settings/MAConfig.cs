fec4df7a83b181beb7453f30388273b52f26a37e
RoGreat <roguegreat@gmail.com>
2020-12-05 13:53:31 -0600

    Turned off debug by default.

--- Settings/MAConfig.cs
+++ Settings/MAConfig.cs
@@ -7,7 +7,7 @@
         public bool Incest { get; set; } = false;
         public bool BecomeRuler { get; set; } = false;
         public bool Cheating { get; set; } = false;
-        public bool Debug { get; set; } = true;
+        public bool Debug { get; set; } = false;
         public string Difficulty { get; set; } = "Easy";
         public string SexualOrientation { get; set; } = "Heterosexual";
     }
