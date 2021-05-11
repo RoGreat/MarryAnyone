9086a8a3d6a07e07f263fd409f6287b1da224be7
RoGreat <roguegreat@gmail.com>
2021-05-11 17:37:40 -0500

    Fixed adoption system and polyamory. Education patched due to one parent issue. Updated the hero strings patch.

---
+++ Patches/Behaviors/EducationCampaignBehaviorPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+
+namespace MarryAnyone.Patches.Behaviors
+{
+    [HarmonyPatch(typeof(EducationCampaignBehavior), "GetHighestThreeAttributes")]
+    internal class EducationCampaignBehaviorPatch
+    {
+        // Crash when there is a parent left out of education
+        // Resort to Main Hero (adoption) when that happens
+        private static void Prefix(ref Hero hero)
+        {
+            if (hero is null)
+            {
+                hero = Hero.MainHero;
+            }
+        }
+    }
+}
