e5fbee9a1b80bbca962c4091647789fa36d1f0e1
OnNeNousDitPasTout <tangimaury@yahoo.fr>
2021-07-07 16:53:33 +0200

    Probleme sur le test Romance.GetCourtedHeroInOtherClan quand le hero n'a pas le clan => mise en place d'un patch

---
+++ Models/MARomanceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace MarryAnyone.Models
+{
+    internal class MARomanceModel : DefaultRomanceModel
+    {
+        public override bool CourtshipPossibleBetweenNPCs(Hero person1, Hero person2)
+        {
+            return Romance.GetRomanticLevel(person1, person2) == Romance.RomanceLevelEnum.Untested 
+                && (person2.Clan == null || Romance.GetCourtedHeroInOtherClan(person1, person2) == null) 
+                && (person1.Clan == null || Romance.GetCourtedHeroInOtherClan(person2, person1) == null) 
+                && Campaign.Current.Models.MarriageModel.IsCoupleSuitableForMarriage(person1, person2);
+        }
+    }
+}
