9cc2386472e0c50fbe54210515dfde37ed046bec
RoGreat <RogueGreat@gmail.com>
2022-08-21 12:49:32 -0500

    Fixed the cheating mechanic

--- MarryAnyone/Helpers/MAHelpers.cs
+++ MarryAnyone/Helpers/MAHelpers.cs
@@ -9,37 +9,68 @@ namespace MarryAnyone.Helpers
 {
     internal static class MAHelpers
     {
-        public static void RemoveExSpouses(Hero hero, bool removeAll = false)
-        {
-            FieldInfo _exSpouses = AccessTools.Field(typeof(Hero), "_exSpouses");
-            List<Hero> _exSpousesList = (List<Hero>)_exSpouses.GetValue(hero);
-            FieldInfo ExSpouses = AccessTools.Field(typeof(Hero), "ExSpouses");
-            MBReadOnlyList<Hero> ExSpousesReadOnlyList;
+        private static FieldInfo ExSpouses = AccessTools.Field(typeof(Hero), "ExSpouses");
 
-            if (removeAll)
-            {
-                // Remove exspouse completely from list
-                _exSpousesList = _exSpousesList.ToList();
-                List<Hero> exSpouses = _exSpousesList.Where(exSpouse => exSpouse.IsAlive).ToList();
-                foreach (Hero exSpouse in exSpouses)
-                {
-                    _exSpousesList.Remove(exSpouse);
-                }
-            }
-            else
+        private static FieldInfo _exSpouses = AccessTools.Field(typeof(Hero), "_exSpouses");
+
+        public static void RemoveExSpouses(Hero hero, bool duplicates = true)
+        {
+            List<Hero> _exSpousesList = (List<Hero>)_exSpouses.GetValue(hero);
+
+            if (duplicates)
             {
                 // Standard remove duplicates spouse
                 // Get exspouse list without duplicates
                 _exSpousesList = _exSpousesList.Distinct().ToList();
-                // If exspouse is already a spouse, then remove it
+                // If exspouse is already a spouse, then remove from exspouses
                 if (_exSpousesList.Contains(hero.Spouse))
                 {
                     _exSpousesList.Remove(hero.Spouse);
+                    MADebug.Print($"Removed duplicate spouse {hero.Spouse.Name.ToString()}");
                 }
             }
-            ExSpousesReadOnlyList = new MBReadOnlyList<Hero>(_exSpousesList);
+            else
+            {
+                // Remove all exspouses
+                _exSpousesList = _exSpousesList.ToList();
+                List<Hero> exSpouses = _exSpousesList.Where(exSpouse => exSpouse.IsAlive).ToList();
+                foreach (Hero exSpouse in exSpouses)
+                {
+                    // Remove exspouse from list
+                    _exSpousesList.Remove(exSpouse);
+
+                    // Look into your exspouse's exspouse to remove yourself
+                    List<Hero> _exSpousesList2 = (List<Hero>)_exSpouses.GetValue(exSpouse);
+                    _exSpousesList2.Remove(hero);
+
+                    MBReadOnlyList<Hero> ExSpousesReadOnlyList2 = new(_exSpousesList2);
+                    _exSpouses.SetValue(exSpouse, _exSpousesList2);
+                    ExSpouses.SetValue(exSpouse, ExSpousesReadOnlyList2);
+                }
+            }
+
+            MBReadOnlyList<Hero> ExSpousesReadOnlyList = new(_exSpousesList);
             _exSpouses.SetValue(hero, _exSpousesList);
             ExSpouses.SetValue(hero, ExSpousesReadOnlyList);
+        }
+
+        public static void CheatOnSpouse()
+        {
+            List<Hero> _exSpousesList = (List<Hero>)_exSpouses.GetValue(Hero.MainHero);
+            List<Hero> cheatedHeroes = _exSpousesList.Where(exSpouse => exSpouse.IsAlive).ToList();
+
+            foreach (Hero cheatedHero in cheatedHeroes)
+            {
+                RemoveExSpouses(cheatedHero, false);
+                if (cheatedHero != Hero.MainHero.Spouse)
+                {
+                    MADebug.Print($"Broke off marriage with {cheatedHero.Name.ToString()}");
+                }
+                else
+                {
+                    MADebug.Print($"Removed duplicate spouse {cheatedHero.Name.ToString()}");
+                }
+            }
         }
     }
 }
